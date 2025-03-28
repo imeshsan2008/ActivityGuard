1129e7b05d82466b8ab1f2818566b2eddc32efc0
Imesh Sandeepa <111564145+imeshsan2008@users.noreply.github.com>
2025-03-28 21:48:05 +0530

    Update ps.bat

--- ps.bat
+++ ps.bat
@@ -1,13 +1,1 @@
-@echo off
-echo Checking for "Activity Guard" process...
-
-taskkill /f /im "Activity Guard.exe"
-
-
-
-echo Waiting for 8 minutes...
-timeout /t 8000 /nobreak >nul
-echo Done.
-
-:END
-pause
+yiu
